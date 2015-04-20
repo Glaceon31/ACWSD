4d05facddd9eb95d0556847bd613f03169c3567b
Glaceon31 <grit31@126.com>
2015-04-20 11:25:39 +0800

    compatible for linux and windows

--- wsdata.py
+++ wsdata.py
@@ -3,7 +3,7 @@ from lxml import etree
 import re
 import os
 import os.path
-
+import platform
 
 def parsexml(xml):
     senses = []
@@ -15,11 +15,16 @@ failnum = 0
 for parents,dirnames,filenames in os.walk(r'dict'):
     for f in filenames:
         #try:
-            word = {}
-            word['senses'] = open('dict//'+f,'rb').read()
+        word = {}
+        word['senses'] = open('dict//'+f,'rb').read()
+        if platform.system() == 'Windows':
             wordlist[os.path.splitext(f)[0].decode('gbk')] = word
             wordnum += 1
             print wordnum ,os.path.splitext(f)[0].decode('gbk')
+        else:
+            wordlist[os.path.splitext(f)[0].decode('utf-8')] = word
+            wordnum += 1
+            print wordnum ,os.path.splitext(f)[0].decode('utf-8')
         #except:
         #    failnum += 1
         #    print 'fail: '+f
