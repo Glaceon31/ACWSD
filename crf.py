887fd12af98248bc3091177dc88551f70c4d198b
Glaceon31 <grit31@126.com>
2015-05-19 14:47:00 +0800

    finish crf test

--- crf.py
+++ crf.py
@@ -46,12 +46,12 @@ def crf(keyword):
         for j in range(0, len(testsentence[i])/2):
             if testsentence[i][j] == ' ':
                 continue
-            testcontent += testsentence[i][j]+' '+'X\n'
-        testcontent += testsentence[i][len(testsentence[i])/2]+' '+senselist[test_set_y[i].eval()]+'\n'
+            testcontent += testsentence[i][j]+' 0 '+'X\n'
+        testcontent += testsentence[i][len(testsentence[i])/2]+' 1 '+senselist[test_set_y[i].eval()]+'\n'
         for j in range(len(testsentence[i])/2+1, len(testsentence[i])):
             if testsentence[i][j] == ' ':
                 continue
-            testcontent += testsentence[i][j]+' '+'X\n'
+            testcontent += testsentence[i][j]+' 0 '+'X\n'
         testcontent += '\n'
 
     output = codecs.open('crf//train//'+keyword+'_crf.txt', 'wb', 'utf-8')
@@ -66,7 +66,41 @@ def crf(keyword):
 
     os.system('crf_learn crf/template crf/train/'+keyword.encode('utf-8')+'_crf.txt crf/model/'+keyword.encode('utf-8')+'_crf')
     time.sleep(1)
-    os.system('crf_test -m crf/model/'+keyword.encode('utf-8')+'_crf crf/test/'+keyword.encode('utf-8')+'_crf.txt')
+    os.system('crf_test -m crf/model/'+keyword.encode('utf-8')+'_crf crf/test/'+keyword.encode('utf-8')+'_crf.txt > crf/output/'+keyword.encode('utf-8')+'_crf.txt')
+
+    #check resuilt
+    num = 0
+    taggednum = 0
+    predictnum = 0
+    rightnum = 0
+    inp = open('crf/output/'+keyword.encode('utf-8')+'_crf.txt', 'rb').read()
+    words = inp.split('\n')
+    print words
+    for word in words:
+        if word == '':
+            continue
+        tokens = word.split('\t')
+        if tokens[0] == keyword.encode('utf-8') and tokens[1] != 'X':
+            num += 1
+            if tokens[2] != 'X':
+                taggednum += 1
+                if tokens[3] != 'X':
+                    predictnum += 1
+                    if tokens[2] == tokens[3]:
+                        rightnum += 1
+
+    print 'tagged: '+str(taggednum)
+    print 'predict: '+str(predictnum)
+    print 'right: '+str(rightnum)
+
+    precision = 1.*rightnum/predictnum
+    recall = 1.*rightnum/taggednum
+    if precision+recall == 0:
+        F = 0.0
+    else:
+        F = 2*precision*recall/(precision+recall)
+    print 'precision: '+str(precision)+' recall: '+str(recall)+' F: '+str(F)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
