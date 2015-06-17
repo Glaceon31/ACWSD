dd1bc7c0ab023ebd32a227603ae58add3ba6ecb8
Glaceon31 <grit31@126.com>
2015-06-17 22:21:49 +0800

    modify

--- wsdata.py
+++ wsdata.py
@@ -11,15 +11,7 @@ dictdb = db.dict
 
 global wordlist
 
-keywordlist = [u'信',u'属',u'之',u'将',u'乃']
-global senselist
-
 def refreshdict():
-	global senselist
-	senselist = {}
-	for keyword in keywordlist:
-		datasets = load_data_word(keyword, 1, 50, sequence = 0)
-		senselist[keyword] = datasets[1]
 	global wordlist
 	wordlist = {}
 	wordnum = 0
